8ba779be0190d0d5ef0269e83ad627e843477d88
Aathmaprakash <aathmaprakash@gmail.com>
2025-02-24 13:43:16 +0530

    changes

--- array-reverse/arrayreverse.cpp
+++ array-reverse/arrayreverse.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
@@ -6,14 +7,15 @@ int main(){
 
         int arr[6]={1,2,3,4,5,6};
 
-        
-       
-                        
+        vector<int>reverse;
 
-
-         for(int i=0;i<6;i++){
-                cout<<arr[i]<<" ";
+        for (int i = 5; i>=0; i--)
+       {
+        reverse.push_back(arr[i]);
+        }
+        for (int i = 0; i<reverse.size(); i++)
+        {
+            cout<<reverse[i]<<" ";
             }
-        return 0;
- }
-
+            return 0;
+        }
