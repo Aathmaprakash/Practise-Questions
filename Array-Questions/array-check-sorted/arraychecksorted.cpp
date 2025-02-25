45cb0d2d97521a12149ae8cf82b6ab8a047cf53c
Aathmaprakash <aathmaprakash@gmail.com>
2025-02-25 09:18:42 +0530

    code

---
+++ Array-Questions/array-check-sorted/arraychecksorted.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int arr[6]={1,2,5,6,8,0};
+
+    if (arr[0]>arr[5])
+    {
+       cout<<"False"<<endl;
+    }
+    else{
+        cout<<"True"<<endl;
+    }
+    
+
+    return 0;
+}
