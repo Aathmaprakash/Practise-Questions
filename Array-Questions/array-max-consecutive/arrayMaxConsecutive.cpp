86a488c7b2efc188cb2b6b8e23563a0986c0ccb8
Aathmaprakash <aathmaprakash@gmail.com>
2025-02-25 09:56:02 +0530

    code

---
+++ Array-Questions/array-max-consecutive/arrayMaxConsecutive.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() { 
+    int arr[7] = {1, 1, 0, 1, 1, 1, 1};
+
+    int count = 0; 
+    int maxi = 0;  
+
+    for (int i = 0; i < 7; i++) {
+        if (arr[i] == 1) {
+            count++; 
+            maxi = max(maxi, count);
+        } else {
+            count = 0; 
+        }
+    }
+
+    cout << "Max consecutive 1s: " << maxi << endl;
+
+    return 0;
+}
