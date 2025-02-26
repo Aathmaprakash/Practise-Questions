c3a4bf339fa2ed080e739111ebcd40131da91cb7
Aathmaprakash <aathmaprakash@gmail.com>
2025-02-26 12:31:50 +0530

    code

---
+++ Array-Questions/array-rotate/arrayRotate.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Function to reverse a portion of the array
+void reverseArray(int arr[], int start, int end) {
+    while (start < end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    int k = 3;
+    int n = 6;
+
+   
+    k = k % n;  
+
+   
+    reverseArray(arr, 0, n - 1);
+
+   
+    reverseArray(arr, 0, k - 1);
+
+   
+    reverseArray(arr, k, n - 1);
+
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
