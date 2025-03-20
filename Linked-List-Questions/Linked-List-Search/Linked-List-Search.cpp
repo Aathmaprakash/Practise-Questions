be09a6e480529450182873f30a743fb244e17c06
Aathmaprakash <aathmaprakash@gmail.com>
2025-03-20 10:39:12 +0530

    code

--- Linked-List-Questions/Linked-List-Search/Linked-List-Search.cpp
+++ Linked-List-Questions/Linked-List-Search/Linked-List-Search.cpp
@@ -5,11 +5,58 @@ using namespace std;
 struct Node{
 int data;
 struct Node* next;
+
+Node(int val){
+    data=val;
+    next=nullptr;
 }
+};
+
+class LinkedList{
+    public:
+    Node *head;
+    LinkedList(){
+        head=nullptr;
+    }
+
+    void insert(int val){
+        Node *newNode=new Node(val);
+        newNode->next=head;
+        head=newNode;
+    }
+
+
+    void search(int val){
+        Node *temp=head;
+        while (temp!=NULL)
+        {
+            if(temp->data==val){
+                cout<<"key "<<val<< " is present in the list"<<endl;
+                return;
+            }
+            temp = temp->next;
+            
+               
+            
+        }
+        cout<<"key"<<val<<"is not present in the list"<<endl;
+        
+
+      
+
+       
+    }
+
+};
+
 
 
 int main(){
-    struct Node* head = NULL;
+    LinkedList list;
+    list.insert(10);
+    list.insert(20);
+    list.insert(30);
+    list.search(30);
 
         
 
